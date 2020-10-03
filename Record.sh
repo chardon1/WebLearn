cbf6096536706e2e12fa52e17f1dcd1bb8ae4eb3
aksh-sharma-cp2 <65637457+aksh-sharma-cp2@users.noreply.github.com>
2020-10-03 18:20:32 +0530

    file for records

---
+++ Record.sh
@@ -0,0 +1,36 @@
+ Name : kartik 
+	Gmail : 3 
+ Class : 10 
+ College : sggs 
+ REGISTRATION ID : 3 
+	PASSWORD : kartik
+ Name : kartik 
+	Gmail : kalia 
+ Class : bsc I 
+ College : sggs 
+ REGISTRATION ID : kalia 
+	PASSWORD : kartik
+ Name : kartik 
+	Gmail : kartik 
+ Class : bsc1 
+ College : sggs 
+ REGISTRATION ID : kartik 
+	PASSWORD : kartik
+ Name : kartik 
+	Gmail : kart 
+ Class :  
+ College :  
+ REGISTRATION ID : kart 
+	PASSWORD : 
+ Name : k 
+	Gmail :  
+ Class :  
+ College :  
+ REGISTRATION ID :  
+	PASSWORD : 
+ Name : katik 
+	Gmail : kar01 
+ Class : msc 1st 
+ College : sggs 
+ REGISTRATION ID : kar01 
+	PASSWORD : kartik
